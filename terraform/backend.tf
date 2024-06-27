d44d8957ee27d1727446afa621e9668963f25cae
Denis Nikonorov <mrmxssclxck@yandex.ru>
2024-06-27 22:36:06 +0400

    edit backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,6 +6,6 @@ terraform {
 		key = "s3-bucket-mxssclxck/terraform.tfstate"
 		skip_region_validation = true
 		skip_credentials_validation = true
-		skip_metadata_api_check = true
+		skip_metadata_api_check = false
 	}
 }
